c1d37b8b57b53d0357a39456369ed1c7e7e3cd4c
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-12-28 12:26:35 +0900

    Fix annocation is invalid

--- src/main/kotlin/com/example/helloworld/controller/HelloWorldController.kt
+++ src/main/kotlin/com/example/helloworld/controller/HelloWorldController.kt
@@ -1,19 +1,13 @@
 package com.example.helloworld.controller
 
 import com.example.helloworld.service.HelloWorldService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
-import javax.websocket.server.PathParam
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class HelloWorldController(private val service: HelloWorldService) {
 
     @GetMapping("/hello-worlds/{id}")
-    fun getHelloWorld(
-            @PathParam("id") id: Int
-    ) = service.getHelloWorld(id)
+    fun getHelloWorld(@PathVariable("id") id: Int) = service.getHelloWorld(id)
 
     @GetMapping("/hello-worlds")
     fun getHelloWorldList() = service.getHelloWorldList()
