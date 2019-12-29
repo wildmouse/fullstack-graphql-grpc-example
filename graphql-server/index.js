5637629a805c6de6500b39ef519b1bcd3ee1672f
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-12-29 19:32:21 +0900

    Integrate bff and api

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -1,19 +1,60 @@
-const { ApolloServer, gql } = require('apollo-server')
+const {ApolloServer, gql} = require('apollo-server')
+
+const {RESTDataSource} = require('apollo-datasource-rest')
+
+class HelloWorldAPI extends RESTDataSource {
+  constructor() {
+    super()
+    this.baseURL = 'http://localhost:8080/'
+  }
+
+  async getHelloWorld(id) {
+    return this.get(`hello-worlds/${id}`)
+      .then(response => {
+        return response.message
+      })
+      .catch(error => console.error(error))
+  }
+
+  async getHelloWorlds() {
+    return await this.get('hello-worlds')
+      .then(messages => messages.map(it => it.message))
+      .catch(error => console.error(error))
+  }
+}
 
 const typeDefs = gql`
-  type Query { 
-      message: String
-  }
+    type Query {
+        message(id: Int!): String
+        messages: [String!]!
+    }
 `
 
 const resolvers = {
   Query: {
-    message: () => "Hello, World!"
+    message: async (_source, {id}, {dataSources}) => {
+      const message = await dataSources.helloWorldAPI.getHelloWorld(id)
+      console.log(message)
+      return message
+    },
+    messages: async (_source, {}, {dataSources}) => {
+      const messages = await dataSources.helloWorldAPI.getHelloWorlds()
+      console.log(messages)
+      return messages
+    }
   }
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      helloWorldAPI: new HelloWorldAPI()
+    }
+  }
+})
 
-server.listen().then(({ url }) => {
+server.listen().then(({url}) => {
   console.log(`Server ready at ${url}`)
 })
