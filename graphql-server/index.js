952ef320417dbf7d36c839c749f787a616310127
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-01-02 15:03:06 +0900

    Replace datasource from REST to gRPC on apollo server

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -2,10 +2,8 @@ const {ApolloServer, gql} = require('apollo-server')
 
 const {RESTDataSource} = require('apollo-datasource-rest')
 
-const express = require('express')
 const protoLoader = require('@grpc/proto-loader')
 const grpc = require('grpc')
-const bodyParser = require('body-parser')
 
 const greetingProtoPath = '../api/src/main/proto/greeting.proto'
 const greetingProtoDefinition = protoLoader.loadSync(greetingProtoPath)
@@ -15,98 +13,106 @@ const client = new greetingPackageDefinition.GreetingService(
   "localhost:8080", grpc.credentials.createInsecure()
 )
 
-const GetGreetings = (req, res) => {
-  client.GetGreetings({}, (err, result) => {
-    res.json(result)
+const GetGreeting = (params, context) => {
+  return new Promise((resolve, reject) => {
+    client.GetGreeting({id: params.id}, {}, (err, result) => {
+      return resolve(result)
+    })
   })
 }
 
-const grpcRoutes = {
-  GetGreetings
+const GetGreetings = (params, context) => {
+  return new Promise((resolve, reject) => {
+    client.GetGreetings({}, {}, (err, result) => {
+      return resolve(result)
+    })
+  })
 }
 
-const router = express.Router()
+const SaveGreeting = ({message}, context) => {
+  return new Promise((resolve, reject) => {
+    client.SaveGreeting({message}, {}, (err, result) => {
+      return resolve(result)
+    })
+  })
+}
 
-router.get('/greetings', grpcRoutes.GetGreetings)
+// Leave as a sample
+class HelloWorldAPI extends RESTDataSource {
+  constructor() {
+    super()
+    this.baseURL = 'http://localhost:8080/'
+  }
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
+  async getHelloWorld(id) {
+    return this.get(`hello-worlds/${id}`)
+      .then(response => {
+        return response.message
+      })
+      .catch(error => console.error(error))
+  }
 
-app.use('/api', router)
+  async getHelloWorlds() {
+    return await this.get('hello-worlds')
+      .then(messages => messages.map(it => it.message))
+      .catch(error => console.error(error))
+  }
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000')
+  async saveHelloWorld(message) {
+    return await this.post(
+      `hello-worlds?message=${message}`
+    )
+      .then(isSaved => isSaved)
+      .catch(error => console.error(error))
+  }
+}
+
+// TODO: define greeting type
+const typeDefs = gql`
+    type Query {
+        message(id: Int!): String
+        messages: [String!]!
+    }
+    type Mutation {
+        message(message: String!): Boolean
+    }
+`
+
+const resolvers = {
+  Query: {
+    message: async (_source, {id}) => {
+      const {message} = await GetGreeting({id}, (err, result) => {
+        return result
+      })
+      return message
+    },
+    messages: async (_source, {}) => {
+      const {messages} = await GetGreetings({}, (err, result) => {
+        return result
+      })
+      return messages
+    }
+  },
+  Mutation: {
+    message: async (_source, {message}) => {
+      const { isSaved } = await SaveGreeting({message}, (err, result) => {
+        return result
+      })
+      return isSaved
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      helloWorldAPI: new HelloWorldAPI()
+    }
+  }
 })
 
-// class HelloWorldAPI extends RESTDataSource {
-//   constructor() {
-//     super()
-//     this.baseURL = 'http://localhost:8080/'
-//   }
-//
-//   async getHelloWorld(id) {
-//     return this.get(`hello-worlds/${id}`)
-//       .then(response => {
-//         return response.message
-//       })
-//       .catch(error => console.error(error))
-//   }
-//
-//   async getHelloWorlds() {
-//     return await this.get('hello-worlds')
-//       .then(messages => messages.map(it => it.message))
-//       .catch(error => console.error(error))
-//   }
-//
-//   async saveHelloWorld(message) {
-//     return await this.post(
-//       `hello-worlds?message=${message}`
-//     )
-//       .then(isSaved => isSaved)
-//       .catch(error => console.error(error))
-//   }
-// }
-//
-// // TODO: define hello world
-// const typeDefs = gql`
-//     type Query {
-//         message(id: Int!): String
-//         messages: [String!]!
-//     }
-//     type Mutation {
-//         message(message: String!): Boolean
-//     }
-// `
-//
-// const resolvers = {
-//   Query: {
-//     message: async (_source, {id}, {dataSources}) => {
-//       const message = await dataSources.helloWorldAPI.getHelloWorld(id)
-//       return message
-//     },
-//     messages: async (_source, {}, {dataSources}) => {
-//       const messages = await dataSources.helloWorldAPI.getHelloWorlds()
-//       return messages
-//     }
-//   },
-//   Mutation: {
-//     message: async (_source, {message}, {dataSources}) => {
-//       await dataSources.helloWorldAPI.saveHelloWorld(message)
-//     }
-//   }
-// }
-//
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   dataSources: () => {
-//     return {
-//       helloWorldAPI: new HelloWorldAPI()
-//     }
-//   }
-// })
-//
-// server.listen().then(({url}) => {
-//   console.log(`Server ready at ${url}`)
-// })
+server.listen().then(({url}) => {
+  console.log(`Server ready at ${url}`)
+})
