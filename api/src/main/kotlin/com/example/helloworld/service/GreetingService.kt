71cb13c0fc74288461c76fda87f50c87097303ee
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-01-04 10:11:01 +0900

    Update gRPC service to reactive

--- api/src/main/kotlin/com/example/helloworld/service/GreetingService.kt
+++ api/src/main/kotlin/com/example/helloworld/service/GreetingService.kt
@@ -1,71 +1,58 @@
 package com.example.helloworld.service
 
-import com.example.helloworld.grpc.greeting.*
 import com.example.helloworld.entity.Greeting
+import com.example.helloworld.grpc.greeting.*
 import com.example.helloworld.repository.GreetingRepository
-import io.grpc.stub.StreamObserver
+import reactor.core.publisher.Mono
 
 class GreetingGrpcService(
         private val repository: GreetingRepository
-) : GreetingServiceGrpc.GreetingServiceImplBase() {
-    override fun getGreeting(request: GetGreetingRequest?, responseObserver: StreamObserver<GetGreetingResponse>?) {
-        val id = request?.id ?: -1
+) : ReactorGreetingServiceGrpc.GreetingServiceImplBase() {
+    override fun getGreeting(request: Mono<GetGreetingRequest>): Mono<GetGreetingResponse> =
+            request.map {
+                val greeting = repository.findById(it.id)
+                if (greeting.isPresent) {
+                    GetGreetingResponse.newBuilder()
+                            .setGreeting(greeting.let {
+                                com.example.helloworld.grpc.greeting.Greeting
+                                        .newBuilder()
+                                        .setMessage(it.get().message)
+                                        .setId(it.get().id)
+                                        .build()
+                            })
+                            .build()
+                } else {
+                    GetGreetingResponse.getDefaultInstance()
+                }
+            }
 
-        val greeting = repository
-                .findById(id)
+    override fun getGreetings(request: Mono<GetGreetingsRequest>): Mono<GetGreetingsResponse> =
+            request.map {
+                val greetings = repository.findAll().map {
+                    com.example.helloworld.grpc.greeting.Greeting
+                            .newBuilder()
+                            .setMessage(it.message)
+                            .setId(it.id)
+                            .build()
+                }
+                GetGreetingsResponse
+                        .newBuilder()
+                        .addAllGreetings(greetings)
+                        .build()
+            }
 
-        val response = if (greeting.isPresent) {
-            GetGreetingResponse.newBuilder()
-                    .setGreeting(greeting.let {
-                        com.example.helloworld.grpc.greeting.Greeting
-                                .newBuilder()
-                                .setMessage(it.get().message)
-                                .setId(it.get().id)
-                                .build()
-                    })
-                    .build()
-        } else {
-            GetGreetingResponse.getDefaultInstance()
-        }
-
-        responseObserver?.onNext(response)
-        responseObserver?.onCompleted()
-    }
-
-    override fun getGreetings(request: GetGreetingsRequest?, responseObserver: StreamObserver<GetGreetingsResponse>?) {
-        val greetings = repository.findAll().map {
-            com.example.helloworld.grpc.greeting.Greeting
-                    .newBuilder()
-                    .setMessage(it.message)
-                    .setId(it.id)
-                    .build()
-        }
-
-        val response = GetGreetingsResponse
-                .newBuilder()
-                .addAllGreetings(greetings)
-                .build()
-
-        responseObserver?.onNext(response)
-        responseObserver?.onCompleted()
-    }
-
-    override fun saveGreeting(request: SaveGreetingRequest?, responseObserver: StreamObserver<SaveGreetingResponse>?) {
-        val message = request?.message ?: "Hello, World!"
-        val greeting = repository.save(Greeting(message)).let {
-            com.example.helloworld.grpc.greeting.Greeting
-                    .newBuilder()
-                    .setMessage(it.message)
-                    .setId(it.id)
-                    .build()
-        }
-
-        val response = SaveGreetingResponse
-                .newBuilder()
-                .setGreeting(greeting)
-                .build()
-
-        responseObserver?.onNext(response)
-        responseObserver?.onCompleted()
-    }
+    override fun saveGreeting(request: Mono<SaveGreetingRequest>): Mono<SaveGreetingResponse> =
+            request.map {
+                val greeting = repository.save(Greeting(it.message)).let {
+                    com.example.helloworld.grpc.greeting.Greeting
+                            .newBuilder()
+                            .setMessage(it.message)
+                            .setId(it.id)
+                            .build()
+                }
+                SaveGreetingResponse
+                        .newBuilder()
+                        .setGreeting(greeting)
+                        .build()
+            }
 }
